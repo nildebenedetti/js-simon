803181500e80544c49d1c0a7a1a2c341d7f3258d
“Nilde <nilde.benedetti@gmail.com>
2026-04-07 16:02:40 +0200

    add: funzione gigante per pull numeri

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// visualizzare in pagina 5 numeri casuali
-// implement function con math random da 1 a 50 per creare numeri
-// creo * 5 volte un li con il numero myRandom
+// visualizzare in pagina 5 numeri casuali --ok
+// implement function con math random da 1 a 50 per creare numeri --ok
+// creo * 5 volte un li con il numero myRandom --ok
 // metto un timer di 30 secondi (intanto vanno bene 3 per vedere che funzioni)
 // metto d-none ai numeri originali e lo levo al form
 // quando utente clicca submit, controllo che indipendentemente dall'ordine
@@ -18,25 +18,36 @@ const numbersRange = [];
 
 //==================================================================
 
-// invocazione funzione per generare 5 random numbers
-const myFiveNumbers = myRandomNumbersGenerator();
+// racchiudo tutti in funzione di modo che si crei interfaccia utente in un colpo solo
 
-// creo funzione per scrivere un nuovo li con dentro
-// value di myFiveNumbers
-// il tutto per 5 volte
+function populateNumbers() {
 
-for (let i = 0; i <= myFiveNumbers.length -1; i++) {
-   let currentNumber = myFiveNumbers[i];
-    // scrtivimi un li con dentro value di currentNumber
-    let newLi = document.createElement('li');
-     // appendilo
-    numbersList.appendChild(newLi);
-    // add classe
-    numbersList.lastElementChild.classList.add(`number-${i}`);
-    // add html con numero i dell array 
-    newLi.innerHTML = `${myFiveNumbers[i]}`
+
+    // invocazione funzione per generare 5 random numbers
+    const myFiveNumbers = myRandomNumbersGenerator();
+
+    // creo funzione per scrivere un nuovo li con dentro
+    // value di myFiveNumbers
+    // il tutto per 5 volte
+
+    for (let i = 0; i <= myFiveNumbers.length - 1; i++) {
+        let currentNumber = myFiveNumbers[i];
+        // scrtivimi un li con dentro value di currentNumber
+        let newLi = document.createElement('li');
+        // appendilo
+        numbersList.appendChild(newLi);
+        // add classe
+        numbersList.lastElementChild.classList.add(`number-${i}`);
+        // add html con numero i dell array 
+        newLi.innerHTML = `${myFiveNumbers[i]}`
+    }
+
 }
-console.log(numbersList);
+
+// invocazione funzione populate numbers
+
+const newPull = populateNumbers();
+
 
 
 
