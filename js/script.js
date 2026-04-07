ed36f6f2dbb81509e171eaead8c123e42e9745b0
“Nilde <nilde.benedetti@gmail.com>
2026-04-07 15:00:09 +0200

    add processo in script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// visualizzare in pagina 5 numeri casuali
+// implement function con math random da 1 a 50 per creare numeri
+// creo * 5 volte un li con il numero myRandom
+// metto un timer di 30 secondi (intanto vanno bene 3 per vedere che funzioni)
+// metto d-none ai numeri originali e lo levo al form
+// quando utente clicca submit, controllo che indipendentemente dall'ordine
+// i numeri di input matchino quelli generati da myRandom
